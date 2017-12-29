11ad258407605e746ed7b0d71b040c5aed966f46
Solomon Foster <solomonfoster@Solomons-MacBook-Air.local>
2017-12-29 15:33:52 -0500

    functioning tile visualization

--- visualization/tilingVis.js
+++ visualization/tilingVis.js
@@ -1,9 +1,8 @@
-var numSideElements = 3;
+var numSideElements = 5;
 var width = numSideElements * 150;
 var height = numSideElements * 150;
 var fills = ['orange', 'purple', 'green'];
-var testvar;
-var testArray = [0,0,1,0,1,1,2,2,2];
+var testArray = [0,0,0,0,2,0,4,4,4,2,1,1,3,4,2,1,1,3,4,2,1,3,3,3,2];
 var mapsize = testArray.length;
 var distinctTiles = [];
 for (i = 0; i < testArray.length; i++) {
@@ -11,39 +10,20 @@ for (i = 0; i < testArray.length; i++) {
     distinctTiles.push(testArray[i])
   }
 }
-var testvar = [];
-var indexTest = [];
-var coordinateTest;
-var coordinateLists = [];
+testOrderedPoints = [[[0, 0], [125, 0], [150, 0], [275, 0], [300, 0], [425, 0], [450, 0], [575, 0], [575, 125], [450, 125], [425, 125], [300, 125], [275, 125], [150, 125], [125, 125], [125, 150], [125, 275], [0, 275], [0, 150], [0, 125]], [[0, 300], [125, 300], [150, 300], [275, 300], [275, 425], [275, 450], [275, 575], [150, 575], [125, 575], [125, 600], [125, 725], [0, 725], [0, 600], [0, 575], [0, 450], [0, 425], [125, 425], [150, 425], [150, 450], [125, 450]], [[600, 0], [725, 0], [725, 125], [725, 150], [725, 275], [725, 300], [725, 425], [725, 450], [725, 575], [725, 600], [725, 725], [600, 725], [600, 600], [600, 575], [600, 450], [600, 425], [600, 300], [600, 275], [600, 150], [600, 125]], [[300, 300], [425, 300], [425, 425], [425, 450], [425, 575], [425, 600], [450, 600], [575, 600], [575, 725], [450, 725], [425, 725], [300, 725], [275, 725], [150, 725], [150, 600], [275, 600], [300, 600], [300, 575], [300, 450], [300, 425]], [[150, 150], [275, 150], [300, 150], [425, 150], [450, 150], [575, 150], [575, 275], [575, 300], [575, 425], [575, 450], [575, 575], [450, 575], [450, 450], [450, 425], [450, 300], [450, 275], [425, 275], [300, 275], [275, 275], [150, 275]]];
 
-var formatDistrict = function(districtID) {
-  startIndex = testArray.indexOf(districtID);
-  startSpotX = ((startIndex % (mapsize ** .5))*150);
-  startSpotY = (parseInt(startIndex / (mapsize ** .5))*150);
-
-  tileIndices = [];
-  for (i = 0; i < testArray.length; i++) {
-    if (testArray[i] == districtID) {
-      tileIndices.push([i % (mapsize ** .5), parseInt(i / (mapsize ** .5))]);
+var searchArrayForCoordinates = function(coordinates, checklist) {
+  for (i = 0; i < checklist.length; i++) {
+    if ((checklist[i][0] == coordinates[0]) && (checklist[i][1] == coordinates[1])) {
+      return true;
     }
   }
-  coordinateString = startSpotX.toString() + "," + startSpotY.toString() + " ";
-  coordinateList = []
-  for (i = 0; i < tileIndices.length; i++) {
-    coordinateList.push([tileIndices[i][0] * 150, tileIndices[i][1] * 150]);
-    coordinateList.push([tileIndices[i][0] * 150 + 125, tileIndices[i][1] * 150]);
-    coordinateList.push([tileIndices[i][0] * 150 + 125, tileIndices[i][1] * 150 + 125]);
-    coordinateList.push([tileIndices[i][0] * 150, tileIndices[i][1] * 150 + 125]);
-    coordinateTest = coordinateList;
-  }
+  return false;
+}
 
-  neworder = [coordinateList[0]];
-
-
-
-
-
-
+var formatDistrict = function(districtID) {
+  neworder = testOrderedPoints[districtID];
+  coordinateString = "";
   for (i = 0; i < neworder.length; i++) {
     coordinateString += neworder[i][0].toString() + "," + neworder[i][1].toString() + " ";
   }
---
+++ visualization/traversal.py
@@ -0,0 +1,78 @@
+firsttest = [[0,0],[125,0],[150,0],[125,125],[0,125],[150,125],[275,125],[275,0],[0,150],[0,275],[125,150],[125,275]]
+secondtest = [[0, 300],[125, 300],[125, 425],[0, 425],[150, 300],[275, 300],[275, 425],[150, 425],[300, 300],[425, 300],[425, 425],[300, 425]]
+
+testmatrix = [0,0,0,0,2,0,4,4,4,2,1,1,3,4,2,1,1,3,4,2,1,3,3,3,2]
+mapsize = len(testmatrix)
+distinctTiles = list(set(testmatrix))
+
+def createVertices(matrix, districtID):
+    startIndex = matrix.index(districtID)
+    startSpotX = ((startIndex % (mapsize ** .5))*150)
+    startSpotY = (int(startIndex / (mapsize ** .5))*150)
+
+    tileIndices = []
+    for i in range(len(matrix)):
+        if testmatrix[i] == districtID:
+            tileIndices.append([i % (mapsize ** .5), int(i / (mapsize ** .5))])
+
+    coordinateString = str(startSpotX) + "," + str(startSpotY) + " "
+    coordinateList = []
+    for i in range(len(tileIndices)):
+        coordinateList.append([int(tileIndices[i][0] * 150), int(tileIndices[i][1] * 150)])
+        coordinateList.append([int(tileIndices[i][0] * 150 + 125), int(tileIndices[i][1] * 150)])
+        coordinateList.append([int(tileIndices[i][0] * 150 + 125), int(tileIndices[i][1] * 150 + 125)])
+        coordinateList.append([int(tileIndices[i][0] * 150), int(tileIndices[i][1] * 150 + 125)])
+
+    return coordinateList
+
+def traverseVertices(vertices):
+    neworder = [vertices[0]]
+    lastmove = "horizontal"
+    for i in range(len(vertices)):
+        vertex = neworder[i]
+        possiblemoves = []
+        for checkvertex in vertices:
+            distance = abs(checkvertex[0] - vertex[0]) + abs(checkvertex[1] - vertex[1])
+            if 0 < distance <= 125 and checkvertex not in neworder:
+                possiblemoves.append(checkvertex)
+
+        shortmovePossible = False
+        for move in possiblemoves:
+            distance = abs(move[0] - vertex[0]) + abs(move[1] - vertex[1])
+            if distance == 25:
+                shortmovePossible = True
+                neworder.append(move)
+                if abs(move[1] - vertex[1]) == 0:
+                    lastmove = "horizontal"
+                else:
+                    lastmove = "vertical"
+                break
+
+        if not shortmovePossible:
+            samedirectionPossible = False
+            for move in possiblemoves:
+                if abs(move[1] - vertex[1]) == 0 and lastmove == "horizontal":
+                    neworder.append(move)
+                    samedirectionPossible = True
+                    break
+                elif abs(move[0] - vertex[0]) == 0 and lastmove == "vertical":
+                    neworder.append(move)
+                    samedirectionPossible = True
+                    break
+
+            if not samedirectionPossible:
+                if possiblemoves:
+                    neworder.append(possiblemoves[0])
+
+    return neworder
+
+
+def main():
+    inorder = []
+    for i in range(len(distinctTiles)):
+        inorder.append(traverseVertices(createVertices(testmatrix, i)))
+
+    print(inorder)
+
+if __name__ == "__main__":
+    main()
