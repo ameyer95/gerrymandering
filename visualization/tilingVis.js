7924a2378a62f9da2fb59786365f17f8adba2b28
Solomon Foster <solomonfoster@solomons-air.home>
2018-01-22 19:36:35 -0600

    vote distributing

--- visualization/tilingVis.js
+++ visualization/tilingVis.js
@@ -2,6 +2,7 @@ var numSideElements = 5;
 var width = numSideElements * 120;
 var height = numSideElements * 120;
 var currentMapIndex = 0;
+var districtData;
 var testArray = [[0,0,0,0,2,0,4,4,4,2,1,1,3,4,2,1,1,3,4,2,1,3,3,3,2],[0,1,1,1,2,0,3,3,1,2,0,3,3,1,2,0,0,1,2,2,4,4,4,4,4]];
 var mapsize = testArray[currentMapIndex].length;
 var distinctTiles = [];
@@ -36,9 +37,9 @@ var formatDistrict = function(districtID) {
 var buildMap = function() {
   d3.selectAll('.mainmap').remove();
 
-  var minDataVal = d3.min(districtVote);
+  var minDataVal = .3;
   var medDataVal = .5;
-  var maxDataVal = d3.max(districtVote);
+  var maxDataVal = .7;
   var minColor = 'crimson';
   var midColor = 'white';
   var maxColor = 'steelblue';
@@ -141,7 +142,7 @@ var buildSidePanel = function() {
 
   svg.append('text')
       .text(function(d) {return String(districtVote[hoveredDistrict])})
-      .attr("x", "85%")
+      .attr("x", "12%")
       .attr("y", "16%")
       .style('fill', 'black')
       .style('font-family', 'courier')
@@ -158,8 +159,8 @@ var buildSidePanel = function() {
       .style('text-anchor', 'middle');
 
   svg.append('text')
-      .text(function(d) {return String(1 - districtVote[hoveredDistrict])})
-      .attr("x", "85%")
+      .text(function(d) {return (1 - districtVote[hoveredDistrict]).toFixed(2)})
+      .attr("x", "12%")
       .attr("y", "28%")
       .style('fill', 'black')
       .style('font-family', 'courier')
@@ -170,11 +171,48 @@ var buildSidePanel = function() {
 buildMap();
 buildSidePanel();
 
+d3.csv('districtings.csv', function(csvData) {
+    districtData = csvData;
+});
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function distributeVoteUniform() {
+  districtVote = [Number((Math.random()).toFixed(2)),Number((Math.random()).toFixed(2)),Number((Math.random()).toFixed(2)),Number((Math.random()).toFixed(2)),Number((Math.random()).toFixed(2))];
+  buildMap();
+}
+function distributeVoteUniformTrimmed() {
+  districtVote = [.5,.5,.5,.5,.5];
+  buildMap();
+}
+function distributeVoteNormal() {
+  tempArr = [];
+  for (i = 0; i < 5; i++) {
+    innerArr = [];
+    for (j = 0; j < 20; j++) {
+      innerArr.push(Number((Math.random()).toFixed(2)))
+    }
+    innerArrSum = 0;
+    for (k = 0; k < innerArr.length; k++) {
+      innerArrSum = innerArrSum + innerArr[k]
+    }
+    average = Number((innerArrSum/innerArr.length).toFixed(2));
+    tempArr.push(average);
+  }
+  districtVote = tempArr;
+  buildMap();
+}
+function distributeVoteNormalTrimmed() {
+  districtVote = [.5,.5,.5,.5,.5];
+  buildMap();
+}
+function distributeVoteBiased() {
+  districtVote = [.5,.5,.5,.5,.5];
+  buildMap();
 }
 
 // Close the dropdown menu if the user clicks outside of it
